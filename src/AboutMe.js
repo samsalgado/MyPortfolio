486ca588204dd016ad8da6b71ccfc5270519fcc8
samsalgado <samueljuansalgado@gmail.com>
2024-03-05 18:05:24 -0500

    update

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,9 +5,11 @@ const AboutMe = () => {
     <div className='contain'>
         <img src={Me} position='absolute' width='390' height={350} />
         <h1>About Me</h1>
-        <p>My name is Samuel Salgado, a full-stack developer experienced in the niche of Data Science and Blockchain.</p>
-             <p>I started my career in Marketing, so I have a holistic viewpoint on designing business infrastructure for growth.</p>
-                 <p>I am a strategist, primed to provide innovative ideas and implementations for your next project.
+        <p>My name is Samuel Salgado, a full-stack developer experienced in the niche of Data Science and Blockchain.
+</p>
+<p>I have experience in sales and marketing, specifically in SEO, crafting WordPress and other no-code sites, and integrating marketing solutions for business growth.</p>
+
+<p>I am a strategist, ready to provide innovative ideas and implementations for your next project.
 </p>
     </div>
   )
--- src/Table.js
+++ src/Table.js
@@ -6,8 +6,9 @@ const Table = () => {
             languages: 'JavaScript, C++, Golang, SQL, R, Python, Solidity',
             experience: 4,
             projects: 'React Native: Assisting Entrepreneurs, Python: NFL Python Bot, Solidity: PersonNFT, SCP, Golang: Medical Database Contract [HyperLedger]',
-            other_skills: 'SEO, Ahrefs, WordPress Management, Moz, Power BI, Tableau, Drupal, AWS, Marketing Strategy, Supply Chain Sales, Problem-Solving, Instructional Design',
-            certificates: 'R, Solidity, Power BI, JavaScript, Python, Hadoop, Big Data',
+            marketing: 'Yoast, Ahrefs, Google Analytics, Google Search Console, Google Tag Manager, WordPress SEO, Moz',
+            other_skills: 'SEO, Prompt Engineering, CMS Management, Power BI, Tableau, Drupal, AWS, Marketing Strategy, Supply Chain Sales, Problem-Solving, Instructional Design, Customer Service, Consulting',
+            certificates: 'R, Google Adwords,  Solidity, Power BI, JavaScript, Python, Hadoop, Big Data',
             Fields: 'E-commerce, NFT, Supply Chain, Wellness, Blockchain, Cybersecurity'
         }
     ]);
@@ -20,6 +21,7 @@ const Table = () => {
                         <th>Languages</th>
                         <th>Years of Experience</th>
                         <th>Projects</th>
+                        <th>Marketing Skills</th>
                         <th>Other Skills</th>
                         <th>Certificates</th>
                         <th>Fields</th>
@@ -29,6 +31,7 @@ const Table = () => {
                     <td>{stats.languages}</td>
                     <td>{stats.experience}</td>
                     <td>{stats.projects}</td>
+                    <td>{stats.marketing}</td>
                     <td>{stats.other_skills}</td>
                     <td>{stats.certificates}</td>
                     <td>{stats.Fields}</td>
